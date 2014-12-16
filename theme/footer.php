7624c23c2856abff477d431c3450091680227a76
Aaron Pedersen <aaron@aaronmadethis.com>
2014-12-16 16:02:44 -0500

    Added the footer back in with a new design, also some general CSS bug fixes.

--- theme/footer.php
+++ theme/footer.php
@@ -1,8 +1,88 @@
 <footer>
 
+	<?php if(is_front_page() || is_home() ): ?>
+
+	<?php else : ?>
+		<section class="container">
+			<div class="row mega-menu">
+				<div class="logo-wrapper col-xs-4">
+					<a href="<?php echo home_url(); ?>"><div class="logo"></div></a>
+				</div>
+				<div class="col-xs-5 wasserco_p">
+					<div class="inside">
+						<h1>Wasserstein Partners</h1>
+						<ul>
+							<?php
+									$args = array(
+									'include'     => 10,
+									'depth'        => 0,
+									'echo'         => 1,
+									'post_type'    => 'page',
+									'post_status'  => 'publish',
+									'sort_column'  => 'menu_order, post_title',
+								    'sort_order'   => '',
+								    'title_li'     => __(''), 
+								);
+								wp_list_pages( $args );
+							?>
+							<?php
+								$args = array(
+									'child_of'     => 10,
+									'depth'        => 0,
+									'echo'         => 1,
+									'post_type'    => 'page',
+									'post_status'  => 'publish',
+									'sort_column'  => 'menu_order, post_title',
+								    'sort_order'   => '',
+								    'title_li'     => __(''), 
+								);
+								wp_list_pages( $args );
+							?>
+						</ul>
+					</div>
+				</div>
+
+				<div class="col-xs-3 wasserco_do">
+					<div class="inside">
+						<h1>Wasserstein Debt Opportunities</h1>
+						<ul>
+							<?php
+									$args = array(
+									'include'     => 28,
+									'depth'        => 0,
+									'echo'         => 1,
+									'post_type'    => 'page',
+									'post_status'  => 'publish',
+									'sort_column'  => 'menu_order, post_title',
+								    'sort_order'   => '',
+								    'title_li'     => __(''), 
+								);
+								wp_list_pages( $args );
+							?>
+							<?php
+								$args = array(
+									'child_of'     => 28,
+									'depth'        => 0,
+									'echo'         => 1,
+									'post_type'    => 'page',
+									'post_status'  => 'publish',
+									'sort_column'  => 'menu_order, post_title',
+								    'sort_order'   => '',
+								    'title_li'     => __(''), 
+								);
+								wp_list_pages( $args );
+							?>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</section>
+	<?php endif; ?>
+
+
 	<div class="copyright">
 		<div class="container no-pad">
-			<?php the_field('copyright', 'option'); ?>
+			<?php the_field('copyright', 'option'); ?>&nbsp; | &nbsp;<span><a href="<?php echo esc_url( get_permalink( get_page_by_title( 'Disclaimer' ) ) ); ?>">Terms of Use</a></span>
 		</div>
 	</div>
 
